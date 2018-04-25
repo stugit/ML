e55c7628007b931f9798537e6ce7fa52d5fb2d9f
ktang <stugit@users.noreply.github.com>
2018-04-25 22:40:22 +0100

    fixed

--- vector_vector_projection.py
+++ vector_vector_projection.py
@@ -6,5 +6,5 @@ import numpy as np
 # Scalar Projection
 r = np.array([3, -4, 0]) 
 s = np.array([10, 5, -6]) 
-vrs = r * np.vdot(r, s) / np.linalg.norm(r) 
+vrs = r * np.vdot(r, s) / np.linalg.norm(r) ** 2
 print(vrs)
